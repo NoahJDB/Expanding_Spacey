02910bc59f21d187c4c921cf635b028a45d05597
Daan Ruting <daan.j.ruting@gmail.com>
2017-05-22 11:54:44 +0200

    movement (work in prorgess)

---
+++ SpaceGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private bool jumping;
+	private BoxCollider _playerCollider;
+	private Rigidbody _rigidBody;
+	private platform _platform;
+    private float x;
+
+	void Awake(){
+		_playerCollider = GetComponent<BoxCollider>();
+		_rigidBody = GetComponent<Rigidbody>();
+		_platform = GetComponent<platform>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (Physics.Raycast(transform.position, Vector3.down, 1))
+        {
+            _rigidBody.AddForce(Input.GetAxis("Horizontal") * 10, 0, 0);
+        }
+        if (Input.GetButtonDown("Jump") && Physics.Raycast(transform.position, Vector3.down, 1))
+		{
+			_rigidBody.AddForce (Vector3.up * 300);
+            _rigidBody.AddForce (Input.GetAxis("Horizontal")*2,0,0);
+		}
+	}
+}
---
+++ SpaceGame/Assets/Scripts/platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform : MonoBehaviour {
+
+	private BoxCollider2D _platformCollider;
+
+	void Awake	() {
+		_platformCollider = GetComponent<BoxCollider2D>();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
